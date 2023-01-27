72c278449fd2cb30b9cda6eecc144c57ccc7db14
bseyi <bseyi39@gmail.com>
2023-01-27 15:02:19 -0600

    Initial commit

---
+++ app/src/main/java/com/example/smartcitiesapp/StartingPage/Flow2Activity.java
@@ -0,0 +1,16 @@
+package com.example.smartcitiesapp.StartingPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartcitiesapp.R;
+
+public class Flow2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flow2);
+    }
+}
