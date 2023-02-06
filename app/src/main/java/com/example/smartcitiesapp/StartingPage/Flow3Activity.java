bc3a347f69e487cfead72b5e4f2bfb0e8cb92ec6
oledibefrancis <somtofrancis380@gmail.com>
2023-02-06 00:50:39 -0600

    Added the the landing and starting page

--- app/src/main/java/com/example/smartcitiesapp/StartingPage/Flow3Activity.java
+++ app/src/main/java/com/example/smartcitiesapp/StartingPage/Flow3Activity.java
@@ -2,15 +2,33 @@ package com.example.smartcitiesapp.StartingPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.smartcitiesapp.R;
 
 public class Flow3Activity extends AppCompatActivity {
 
+    Button continueButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flow3);
+        continueButton = findViewById(R.id.continueBtn3);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(Flow3Activity.this, Flow3Activity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        });
+
     }
+
 }
